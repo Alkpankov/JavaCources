e5261586767dddb646519b2dd95cc1e04a0b4fd5
Alkpankov <alkpankov@gmail.com>
2020-09-16 19:41:38 +0300

    Не успеваю сделать в срок

---
+++ src/main/java/ru/qeekbrains/courses/java/HomeWork_8/TicTacToeWisual.java
@@ -0,0 +1,21 @@
+package ru.qeekbrains.courses.java.HomeWork_8;
+import java.util.Arrays;
+import java.util.Random;
+import java.util.Scanner;
+public class TicTacToeWisual {
+    private static final char DOT_EMPTY = '•';
+    private static final char DOT_X = 'X';
+    private static final char DOT_O = 'O';
+
+    private static final int SIZE = 5;
+    private static final int DOTS_TO_WIN = 4;
+
+    private static final char[][] map = new char[SIZE][SIZE];
+    private static final Scanner scanner = new Scanner(System.in);
+    private static final Random random = new Random();
+    private static []int[] a  = new int[][];
+
+    public static void main(String[] args) {
+    }
+
+}
