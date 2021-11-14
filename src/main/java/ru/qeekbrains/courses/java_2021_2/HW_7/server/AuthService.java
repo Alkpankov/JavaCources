1fbf5f13f0f0278f8e6dba628861fa6e17927b65
Alkpankov <alkpankov@gmail.com>
2021-11-14 19:02:02 +0300

    HW7

--- src/main/java/ru/qeekbrains/courses/java_2021_2/HW_7/server/AuthService.java
+++ src/main/java/ru/qeekbrains/courses/java_2021_2/HW_7/server/AuthService.java
@@ -1,4 +1,29 @@
 package ru.qeekbrains.courses.java_2021_2.HW_7.server;
 
-public class AuthService {
+/**
+ * сервис аутеннтификации
+ */
+
+public interface AuthService {
+
+    /**
+     * Запустить серис
+     */
+
+    void start();
+
+    /**
+     * Отключить сервис
+     */
+
+    void stop();
+
+    /**
+     * Получить никнейм
+     * @param login
+     * @param pass
+     * @return никнейм если найден или null, если такого нет
+     */
+
+    String getNickByLoginAndPass(String login, String pass);
 }
--- src/main/java/ru/qeekbrains/courses/java_2021_2/HW_7/server/ServerApp.java
+++ src/main/java/ru/qeekbrains/courses/java_2021_2/HW_7/server/ServerApp.java
@@ -1,4 +1,7 @@
 package ru.qeekbrains.courses.java_2021_2.HW_7.server;
 
 public class ServerApp {
+    public static void main(String[] args) {
+        new MyServer();
+    }
 }
